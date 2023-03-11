b4d36015be2f810d4f98cd09d8d8a3ff49368333
kypkk <kypss50102@gmail.com>
2023-03-11 17:21:06 +0800

    feat: homework crawl_stock_info urls_list taiwan50_list

--- homework.py
+++ homework.py
@@ -4,6 +4,7 @@ import time
 import os
 import pymssql
 import concurrent.futures
+from fake_useragent import UserAgent
 from concurrent.futures import ThreadPoolExecutor
 from dotenv import load_dotenv
 from datetime import datetime
@@ -20,11 +21,45 @@ db_settings = {
 }
 
 def crawl_stock_info(url):
+    user_agent = UserAgent()
+    response = requests.get(url=url, headers={ 'user-agent': user_agent.random }).text
+    response = json.loads(response)
+    stock_code = url.split("=")[-1]
+    hint_date = url.split("=")[-2].split("&")[0]
+    for data in response["data"]:
+        [date, tv, t, o, h, l, c, d, v] = data 
+        date = date.split("/")
+        date = str(1911 + int(date[0])) + date[1] + date[2]
 
+        tv = int(tv.replace(",", ""))
+        t = int(t.replace(",", ""))
+        o = float(0.0 if o.replace(',', '') == '--' else o.replace(',', ''))
+        h = float(0.0 if h.replace(',', '') == '--' else h.replace(',', ''))
+        l = float(0.0 if l.replace(',', '') == '--' else l.replace(',', ''))
+        c = float(0.0 if c.replace(',', '') == '--' else c.replace(',', ''))
+        d = float(0.0 if d.replace(',', '') == '--' else d.replace(',', ''))
+        v = float(0.0 if v.replace(',', '') == '--' else v.replace(',', ''))
+        print(date, tv, t, o, h, l, c, d, v)
+        try:
+            conn = pymssql.connect(**db_settings)
+            command = "INSERT INTO [dbo].[股價資訊](stock_code, date, tv, t, o, h, l, c, d, v) VALUES (%s, %s, %d, %d, %s, %s, %s, %s, %s, %s)"
+            with conn.cursor() as cursor:
+                cursor.execute(command, (stock_code, date, tv, t, o, h, l, c, d, v))
+                conn.commit()
+                
+        except Exception as e:
+            print(e)    
+    print(f"----------Insertion Successfully---------- date: {hint_date}")
 
 
 # Main Function
 if __name__ == "__main__":
-    
+    urls = []
+    taiwan50 = [2881, 2882, 2891, 1303, 2303, 2308, 2317, 2330, 2412, 2454]
     # Enter the url of website
-
+    for code in taiwan50:
+        for year in range (2021, 2023):
+            for month in range(1, 13):
+                date = str(year)+f"{month:02d}"+"01"
+                url = f'https://www.twse.com.tw/exchangeReport/STOCK_DAY?re%20sponse=json&date={date}&stockNo={code}'
+                urls.append(url)
